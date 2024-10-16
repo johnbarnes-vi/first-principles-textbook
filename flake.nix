d0b4a04f25ecedc3643d1212d464a60b457426b1
johnbarnes-vi <lj502jr@gmail.com>
2024-10-16 18:32:34 -0500

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+description = "A simple Haskell development environment";
+
+inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+outputs = { self, nixpkgs }:
+    let
+    supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in
+    {
+    devShells = forAllSystems (system:
+        let
+        pkgs = nixpkgsFor.x86_64-linux;
+        in
+        {
+        default = pkgs.haskellPackages.developPackage {
+            root = ./.;
+            modifier = drv:
+            pkgs.haskell.lib.addBuildTools drv (with pkgs.haskellPackages;
+                [
+                haskell-language-server
+                ]);
+        };
+        });
+    };
+}
